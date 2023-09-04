de50bedb3d021258050a8cfdc0191eedb8d7bb56
Nicolas Koriakos <nicolaskoriakos01@gmail.com>
2023-09-04 14:57:08 -0300

    first commit

---
+++ src/main/java/com/koTech/roninKan/RoninKanApplication.java
@@ -0,0 +1,13 @@
+package com.koTech.roninKan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoninKanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoninKanApplication.class, args);
+	}
+
+}
